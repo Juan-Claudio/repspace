69a9a1d911f24f1e7617fe772b62648f049a3082
Juan-Claudio <angeelric@gmail.com>
2024-03-04 15:27:08 +0100

    principal components creation

---
+++ src/components/Controls.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Controls extends React.Component
+{
+    render()
+    {
+        return (
+            <div className="Controls-container"></div>
+        )
+    }
+}
---
+++ src/components/MovesView.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class MovesView extends React.Component
+{
+    render()
+    {
+        return (
+            <div className="MovesView-container"></div>
+        )
+    }
+}
